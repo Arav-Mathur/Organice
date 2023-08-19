254617e1f7985ab9ec34c4d908a09186e6cac5a3
12Firestar <anvaymathur007@gmail.com>
2023-08-19 16:00:05 -0400

    Change to the most recent code

--- organice/App.js
+++ organice/App.js
@@ -1,24 +1,27 @@
-import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import Constants from "expo-constants";
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 import MyTab from "./navigation/BottomTabNavigator";
-import NavigationContainer from "@react-navigation/native";
+import MyStack from "./navigation/StackNavigator"
+import NavigationContainer from "@react-navigation/native"
 export default function App() {
-  return <MyTab />;
+  return (
+      <MyStack/>
+  );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1",
+    backgroundColor: '#ecf0f1',
     padding: 8,
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
