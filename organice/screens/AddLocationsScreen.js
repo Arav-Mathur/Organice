158108114656ca4358b323b21148531ec3b06825
Arav-Mathur <aravmathur007@gmail.com>
2023-08-20 21:50:21 -0400

    add styles to the button

--- organice/screens/AddLocationsScreen.js
+++ organice/screens/AddLocationsScreen.js
@@ -23,18 +23,66 @@ const AddLocationsScreen = ({ navigation, route }) => {
         value={newLocation}
         onChangeText={setNewLocation}
       />
-      <TouchableOpacity onPress={handleAddLocation} >
-        <Text>Add Location</Text>
+      <TouchableOpacity style={styles.loginButton} onPress={handleAddLocation} >
+        <Text style={styles.buttonText}>Add Location</Text>
       </TouchableOpacity>
-      <TouchableOpacity
+      <TouchableOpacity style={styles.loginButton}
         onPress={() =>
           navigation.navigate('Kitchen', { locationOptions })
         }
       >
-        <Text>Submit</Text>
+        <Text style={styles.buttonText}>Submit</Text>
         </TouchableOpacity>
     </View>
   );
 };
 
 export default AddLocationsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  loginButton: {
+    width: 352.5,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#8fb913',
+    shadowColor: '#014f00',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+  },
+  },
+  signUpButton: {
+    width: 352.5,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    backgroundColor: '#8fb913',
+    shadowColor: '#014f00',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+  },
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+    alignSelf: 'center',
+    alignItems: 'center',
+  }
+  });
